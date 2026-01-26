84929fb63fb49a201edfc57e0cd0c01ae288c05f
aqua5432 <kanda.mizuki@g.wakayama-u.jp>
2026-01-26 15:12:44 +0900

    [refactor] fpsの分離

---
+++ Project1/Project1/FPS.cpp
@@ -0,0 +1,46 @@
+#include "FPS.h"
+#include "DxLib.h"
+
+FPSClass Fps;
+void FPSClass::FPSInit()
+{
+	FirsttakeTime = GetNowHiPerformanceCount();
+
+	NowtakeTime = FirsttakeTime;
+	OldtakeTime = FirsttakeTime;
+	Deltatime = 0.000001f;
+	FrameCount = 1;
+	Average = 0.0f;
+
+	return;
+}
+
+void FPSClass::FPSCheck() {
+	NowtakeTime = GetNowHiPerformanceCount();
+	Deltatime = (NowtakeTime - OldtakeTime) / MicroSecond;
+	OldtakeTime = NowtakeTime;
+
+	if (FrameCount == GameFPS)
+	{
+		LONGLONG TotalFrameTIme = NowtakeTime - FirsttakeTime;
+		float CalcAverage = static_cast<float>(TotalFrameTIme) / GameFPS;
+		Average = MicroSecond / CalcAverage;
+		FirsttakeTime = GetNowHiPerformanceCount();
+		FrameCount = 1;
+	}
+	else
+	{
+		FrameCount++;
+	}
+	return;
+}
+
+void FPSClass::FPSWait() {
+	int wait = 0;
+	wait = static_cast<int>(((MicroSecond / GameFPS * FrameCount) - (NowtakeTime - FirsttakeTime)) / MillSecond);	/* waitŽžŠÔ(msec) = —‘z‚ÌŽžŠÔ - ŽÀÛ‚ÌŽžŠÔ */
+
+	if (wait > 0 && wait <= WaitTimeMill) {
+		WaitTimer(wait);
+	}
+	return;
+}
--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -14,6 +14,7 @@
 #include "Scene.h"
 #include "Font.h"
 #include "Color.h"
+#include "FPS.h"
 
 // ƒvƒƒOƒ‰ƒ€‚Í WinMain ‚©‚çn‚Ü‚è‚Ü‚·
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
