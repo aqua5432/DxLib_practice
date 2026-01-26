925c21dd360a51546256f3a777c86a5c61abd783
aqua5432 <kanda.mizuki@g.wakayama-u.jp>
2026-01-26 23:00:24 +0900

    [refactor] Action.hの分離

--- Project1/Project1/src/core/Scene.h
+++ Project1/Project1/src/core/Scene.h
@@ -1,29 +1,18 @@
 #pragma once
+#include "../config/ProjConf.h"
+#include "../game/Action.h"
 
 /*** Sceneクラス ***/
-class
+class SceneClass
 {
 public:
 	/* シーン出力 */
-	void Out()
-	{
-		switch (now)
-		{
-		case SCE::SCE_00_TIT:
-			/* タイトルシーン出力 */
-			now = Tit.Out();
-			break;
-		case SCE::SCE_01_ACT:
-			/* アクションシーン出力 */
-			now = Act.Out();
-			break;
-		default:
-			break;
-		}
-	}
-
+	void Out();
 private:
 	// 現在のシーン
 	int now = SCE::SCE_00_TIT;
+	Action Act;
 
-}Sce;
+};
+
+extern SceneClass Sce;
